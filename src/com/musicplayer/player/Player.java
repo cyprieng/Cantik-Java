727a2aed1e6da6cbbcf014fc84f57f535f6b7b5b
Cyprien G <cyprien.guillemot@gmail.com>
2013-10-29 17:40:47 +0100

    Add PlayerLaunchThread class

--- src/com/musicplayer/player/Player.java
+++ src/com/musicplayer/player/Player.java
@@ -1,25 +1,69 @@
 package com.musicplayer.player;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
-
+/**
+ * Class which manage the playing of music files
+ * 
+ * @author cyprien
+ * 
+ */
 public class Player extends Application {
+	/**
+	 * Path of the file to read
+	 */
+	private static String file;
+
+	/**
+	 * Store the MediaPlayer
+	 */
+	private static MediaPlayer mediaPlayer;
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			String bip = "file:///test.mp3";
-			Media hit = new Media(bip);
-			MediaPlayer mediaPlayer = new MediaPlayer(hit);
-			mediaPlayer.play();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
 	}
-	
-	public static void play() {
+
+	/**
+	 * Get MediaPlayer and init it if necesssry
+	 * 
+	 * @return MediaPlayer initialized with file
+	 */
+	public static MediaPlayer getMediaPlayer() {
+		if (mediaPlayer == null) {
+			// Launch thread to launch "launcher" method
+			PlayerLaunchThread pc = new PlayerLaunchThread();
+			pc.start();
+
+			try {
+				// Load file
+				String fileToLoad = "file:///" + Player.file;
+				Media m = new Media(fileToLoad);
+				Player.mediaPlayer = new MediaPlayer(m);
+			} catch (Exception e) {
+			}
+		}
+		return mediaPlayer;
+	}
+
+	/**
+	 * Set the file value
+	 * 
+	 * @param file
+	 *            path to the song to play
+	 */
+	public static void setFile(String file) {
+		Player.file = file.replace("\\", "/");
+	}
+
+	/**
+	 * Start launch function
+	 * 
+	 * @see javafx
+	 */
+	public void launcher() {
 		launch();
 	}
 }
---
+++ src/com/musicplayer/player/PlayerLaunchThread.java
@@ -0,0 +1,17 @@
+package com.musicplayer.player;
+
+/**
+ * Thread for launching launcher method of Player
+ * 
+ * @author cyprien
+ * 
+ */
+public class PlayerLaunchThread extends Thread {
+	/**
+	 * Thread run function: launch "launcher" method of Player
+	 */
+	public void run() {
+		Player p = new Player();
+		p.launcher();
+	}
+}
