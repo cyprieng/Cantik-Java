6fa0778332f69865fccc9d4aeaab57456c26c337
Cyprien G <cyprien.guillemot@gmail.com>
2013-10-30 16:35:20 +0100

    Move InvalidFileException from updateID3Tags to Song constructor

--- src/com/musicplayer/core/Song.java
+++ src/com/musicplayer/core/Song.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 import org.farng.mp3.MP3File;
-import org.farng.mp3.TagException;
 import org.farng.mp3.id3.ID3v1;
 import org.farng.mp3.id3.ID3v2_4;
 
@@ -35,20 +34,22 @@ public class Song {
 	 *            path of the song
 	 * 
 	 */
-	public Song(String path) {
-		this.path = path;
+	public Song(String path) throws InvalidFileException {
+		// Test file
+		File song = new File(path);
+		if (!song.exists() || !song.isFile()) {
+			throw new InvalidFileException(path);
+		} else {
+			this.path = path;
 
-		// Default value
-		this.title = new File(path).getName();
-		this.album = "";
-		this.artist = "";
-		this.year = "1970";
-		this.lyric = "";
+			// Default value
+			this.title = new File(path).getName();
+			this.album = "";
+			this.artist = "";
+			this.year = "1970";
+			this.lyric = "";
 
-		// Get ID3 tag
-		try {
 			this.updateID3Tags();
-		} catch (InvalidFileException e) {
 		}
 
 	}
@@ -58,46 +59,40 @@ public class Song {
 	 * 
 	 * @see org.farng.mp3
 	 */
-	public void updateID3Tags() throws InvalidFileException {
-		// Get and test file
-		File song = new File(path);
-		if (!song.exists() || !song.isFile()) {
-			throw new InvalidFileException(path);
-		} else {
-			try {
-				MP3File mp3file = new MP3File(song);
+	public void updateID3Tags() {
+		try {
+			File song = new File(path);
+			MP3File mp3file = new MP3File(song);
 
-				// Get ID3 tags
-				if (mp3file.hasID3v1Tag()) {
-					ID3v1 tag = new ID3v1(mp3file.getID3v1Tag());
+			// Get ID3 tags
+			if (mp3file.hasID3v1Tag()) {
+				ID3v1 tag = new ID3v1(mp3file.getID3v1Tag());
 
-					this.title = tag.getSongTitle();
-					this.album = tag.getAlbumTitle();
-					this.artist = tag.getLeadArtist();
-					this.year = tag.getYearReleased();
+				this.title = tag.getSongTitle();
+				this.album = tag.getAlbumTitle();
+				this.artist = tag.getLeadArtist();
+				this.year = tag.getYearReleased();
 
-					try {
-						this.lyric = tag.getSongLyric();
-					} catch (UnsupportedOperationException e) {
-					}
-
-				} else if (mp3file.hasID3v2Tag()) {
-					ID3v2_4 tag = new ID3v2_4(mp3file.getID3v2Tag());
-
-					this.title = tag.getSongTitle();
-					this.album = tag.getAlbumTitle();
-					this.artist = tag.getLeadArtist();
-					this.year = tag.getYearReleased();
-
-					try {
-						this.lyric = tag.getSongLyric();
-					} catch (UnsupportedOperationException e) {
-					}
+				try {
+					this.lyric = tag.getSongLyric();
+				} catch (UnsupportedOperationException e) {
 				}
-			} catch (IOException | TagException e) {
-			}
-		}
 
+			} else if (mp3file.hasID3v2Tag()) {
+				ID3v2_4 tag = new ID3v2_4(mp3file.getID3v2Tag());
+
+				this.title = tag.getSongTitle();
+				this.album = tag.getAlbumTitle();
+				this.artist = tag.getLeadArtist();
+				this.year = tag.getYearReleased();
+
+				try {
+					this.lyric = tag.getSongLyric();
+				} catch (UnsupportedOperationException e) {
+				}
+			}
+		} catch (Exception e) {
+		}
 	}
 
 	/**
