8fa4f6784a6d51a8e5d349574c92cac8e2608cea
Cyprien G <cyprien.guillemot@gmail.com>
2013-10-30 12:06:31 +0100

    Update Player class: now managing play/pause and can change the file playing

--- src/com/musicplayer/player/Player.java
+++ src/com/musicplayer/player/Player.java
@@ -22,40 +22,14 @@ public class Player extends Application {
 	 */
 	private static MediaPlayer mediaPlayer;
 
+	/**
+	 * Store the PlayerLaunchThread
+	 * @see PlayerLaunchThread
+	 */
+	private static PlayerLaunchThread plc;
+
 	@Override
 	public void start(Stage primaryStage) {
-	}
-
-	/**
-	 * Get MediaPlayer and init it if necesssry
-	 * 
-	 * @return MediaPlayer initialized with file
-	 */
-	public static MediaPlayer getMediaPlayer() {
-		if (mediaPlayer == null) {
-			// Launch thread to launch "launcher" method
-			PlayerLaunchThread pc = new PlayerLaunchThread();
-			pc.start();
-
-			try {
-				// Load file
-				String fileToLoad = "file:///" + Player.file;
-				Media m = new Media(fileToLoad);
-				Player.mediaPlayer = new MediaPlayer(m);
-			} catch (Exception e) {
-			}
-		}
-		return mediaPlayer;
-	}
-
-	/**
-	 * Set the file value
-	 * 
-	 * @param file
-	 *            path to the song to play
-	 */
-	public static void setFile(String file) {
-		Player.file = file.replace("\\", "/");
 	}
 
 	/**
@@ -65,5 +39,58 @@ public class Player extends Application {
 	 */
 	public void launcher() {
 		launch();
+	}
+
+	/**
+	 * Init MediaPlayer it if necessary
+	 * 
+	 */
+	public static void initMediaPlayer() {
+		if (mediaPlayer == null) {
+			if (plc == null) {
+				// Launch thread to launch "launcher" method
+				plc = new PlayerLaunchThread();
+				plc.start();
+			}
+
+			try {
+				// Load file
+				String fileToLoad = "file:///" + Player.file;
+				Media m = new Media(fileToLoad);
+				Player.mediaPlayer = new MediaPlayer(m);
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	/**
+	 * Set the file value, and launch initialization of MediaPlayer
+	 * 
+	 * @param file
+	 *            path to the song to play
+	 */
+	public static void setFile(String file) {
+		Player.file = file.replace("\\", "/");
+
+		if (Player.mediaPlayer != null) { //Delete actual mediaPlayer
+			Player.mediaPlayer.stop();
+			Player.mediaPlayer = null;
+		}
+
+		Player.initMediaPlayer();
+	}
+
+	/**
+	 * Start playing the player
+	 */
+	public static void play() {
+		Player.mediaPlayer.play();
+	}
+
+	/**
+	 * Pause the player
+	 */
+	public static void pause() {
+		Player.mediaPlayer.pause();
 	}
 }
