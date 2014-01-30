522c165a995fa3d232821bd587fb389840a1185e
Cyprien G <cyprien.guillemot@gmail.com>
2014-01-30 14:11:10 +0100

    The beginnings of the GUI: parameters and search field

---
+++ src/com/musicplayer/gui/GUIParameters.java
@@ -0,0 +1,27 @@
+package com.musicplayer.gui;
+
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ * Parameters of the UI (Color, font...)
+ * 
+ * @author cyprien
+ * 
+ */
+public class GUIParameters {
+	/**
+	 * Global font
+	 */
+	public final static Font FONT = new Font("Verdana", Font.BOLD, 12);
+
+	/**
+	 * Color of borders
+	 */
+	public final static Color BORDER = new Color(0x4c6072);
+
+	/**
+	 * Background color of the left Bar
+	 */
+	public final static Color LEFTBAR_BACKGROUND = new Color(0x12171c);
+}
---
+++ src/com/musicplayer/gui/leftbar/SearchField.java
@@ -0,0 +1,116 @@
+package com.musicplayer.gui.leftbar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.FocusManager;
+import javax.swing.JTextField;
+
+import com.musicplayer.core.Log;
+import com.musicplayer.gui.GUIParameters;
+
+/**
+ * Create a custom JTextField for the search bar
+ * 
+ * @author cyprien
+ * 
+ */
+public class SearchField extends JTextField {
+	private static final long serialVersionUID = -1258623083307335060L;
+
+	/**
+	 * Represent the round rect
+	 */
+	private Shape shape;
+
+	/**
+	 * Contructor: init the field
+	 */
+	public SearchField() {
+		super();
+		this.setPreferredSize(new Dimension(200, 30));
+		this.setMaximumSize(this.getPreferredSize());
+		this.setBackground(GUIParameters.LEFTBAR_BACKGROUND);
+		this.setForeground(Color.WHITE);
+		this.setFont(GUIParameters.FONT);
+		this.setOpaque(false);
+
+		// Repaint when focus change
+		final JTextField search = this;
+		this.addFocusListener(new FocusAdapter() {
+			public void focusGained(FocusEvent e) {
+				search.repaint();
+			}
+
+			public void focusLost(FocusEvent e) {
+				search.repaint();
+			}
+		});
+
+		// Change text position in the field
+		setBorder(javax.swing.BorderFactory.createCompoundBorder(
+				javax.swing.BorderFactory.createTitledBorder(null,
+						"Border Title",
+						javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+						javax.swing.border.TitledBorder.DEFAULT_POSITION,
+						GUIParameters.FONT), javax.swing.BorderFactory
+						.createEmptyBorder(-10, 25, 0, 0)));
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		// Draw round rect
+		g.setColor(getBackground());
+		((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+
+		// Draw the search icon
+		try {
+			BufferedImage img = ImageIO.read(new File("img/search.png"));
+			g.drawImage(img, 5, 5, 20, 21, this);
+		} catch (IOException e) {
+			Log.addEntry(e);
+		}
+
+		super.paintComponent(g);
+
+		// Placeholder
+		if (getText().isEmpty()
+				&& !(FocusManager.getCurrentKeyboardFocusManager()
+						.getFocusOwner() == this)) {
+			Graphics2D g2 = (Graphics2D) g.create();
+			g2.setColor(GUIParameters.BORDER);
+			g2.setFont(getFont().deriveFont(Font.ITALIC));
+			g2.drawString("Search", 30, 20);
+			g2.dispose();
+		}
+	}
+
+	@Override
+	protected void paintBorder(Graphics g) {
+		g.setColor(GUIParameters.BORDER);
+		g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 30, 30);
+	}
+
+	@Override
+	public boolean contains(int x, int y) {
+		if (shape == null || !shape.getBounds().equals(getBounds())) {
+			shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1,
+					getHeight() - 1, 30, 30);
+		}
+		return shape.contains(x, y);
+	}
+}
